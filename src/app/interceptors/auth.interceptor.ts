c8154c1adae220aa28443205795145adc8e16bba
Nataliia Koval <hakunata@gmail.com>
2021-05-03 11:24:01 +0300

    add logs for interceptor

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -12,6 +13,18 @@ export class AuthInterceptor implements HttpInterceptor {
     const modifiedReq = request.clone({
       withCredentials: true
     });
-    return next.handle(modifiedReq);
+
+    return next.handle(modifiedReq)
+      .pipe(
+        tap(val => {
+          if (val.type === HttpEventType.Sent) {
+            console.log('Request was sent to server');
+          }
+
+          if (val.type === HttpEventType.Response) {
+            console.log('Got a response from the API', val);
+          }
+        })
+      );
   }
 }
